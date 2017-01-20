faa9bc8572599b16fc57333f015cb2e22e7f6722
Tais de Souza Lessa <tais@mozillafoundation.org>
2017-01-20 15:14:55 -0800

    all files

---
+++ js/hovereffect.js
@@ -0,0 +1,11 @@
+
+$(function()
+{
+ $('#alexandraimg').hover(function(){
+ 		$('#alexandraimg').attr('src','img/alexandra-card-hover-state.jpg');
+ },
+ function(){
+ $('#alexandraimg').attr('src','img/alexandra-card-normal-state.jpg');
+ })
+
+})
